339c28e338a425a8d4b0530352db687976993c86
Lukasz Wiecek <lukasz.wiecek@ts.fujitsu.com>
2018-11-23 10:32:08 +0100

    Initial validateInteractiveCommands function

--- src/main/java/org/oscm/app/shell/business/ScriptValidator.java
+++ src/main/java/org/oscm/app/shell/business/ScriptValidator.java
@@ -12,12 +12,16 @@ import org.oscm.app.v2_0.exceptions.APPlatformException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.StringReader;
+
 public class ScriptValidator {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(ScriptValidator.class);
 
     public void validate(Configuration configuration, ConfigurationKey scriptKey)
-            throws APPlatformException {
+            throws APPlatformException, IOException {
 
         LOGGER.debug("Validation of script [" + scriptKey + "] started");
 
@@ -52,8 +56,23 @@ public class ScriptValidator {
         }
     }
 
-    public void validateInteractiveCommands(Script script) {
-        //TODO: validate script upon the interactive commands
+    public void validateInteractiveCommands(Script script) throws APPlatformException, IOException {
+        String[] interactiveCommands = {"read", "ssh", "rm -i"};
+
+        String scriptContent = script.getContent();
+        StringReader stringReader = new StringReader(scriptContent);
+        BufferedReader bufferedReader = new BufferedReader(stringReader);
+        String line;
+        while ((line = bufferedReader.readLine()) != null) {
+            for (int i = 0; i < interactiveCommands.length; i++) {
+                if (line.matches("(^\\s*" + interactiveCommands[i] +
+                        "(\\s?\\w+)*)|(^[a-zA-Z0-9_-]+\\s?(\\w*\\s?)*)*(\\|" + interactiveCommands[i] +
+                        "|\\| " + interactiveCommands[i] + ")(\\s?\\w+)*")) {
+                    throw new APPlatformException("Script " + script.getPath() + " " +
+                            "contains an interactive command! Line that caused the exception: " + line);
+                }
+            }
+        }
     }
 
     public void validateEndOfScript(Script script) throws APPlatformException {
