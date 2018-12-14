2eb45153fcf60434b2f8ee5c8247b60d70c6159a
Arkadiusz Kowalczyk <arkadiusz.kowalczyk@ts.fujitsu.com>
2018-12-14 11:51:19 +0100

    Sample script tests added

--- src/test/java/org/oscm/app/shell/business/api/ShellTest.java
+++ src/test/java/org/oscm/app/shell/business/api/ShellTest.java
@@ -9,6 +9,7 @@
 package org.oscm.app.shell.business.api;
 
 import org.junit.Test;
+import org.richfaces.json.JSONException;
 
 import javax.json.JsonObject;
 import java.net.URI;
@@ -18,17 +19,18 @@ import java.nio.file.Paths;
 import java.util.stream.Collectors;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.oscm.app.shell.business.api.Shell.*;
 import static org.oscm.app.shell.business.api.ShellStatus.RUNNING;
 
 public class ShellTest {
 
     @Test
-    public void testGetResultReturnedSuccess() throws Exception {
+    public void testGetResult_isOk_ifReturnedDataIsOk() throws Exception {
 
         //given
         String instanceId = "Instance_1236678329433";
-        String scriptContent = getScriptContent("sample_scripts/status_success.sh");
+        String scriptContent = getScriptContent("sample_scripts/sample_ok.sh");
         Shell shell = runScript(scriptContent, instanceId);
 
         //when
@@ -39,11 +41,27 @@ public class ShellTest {
     }
 
     @Test
-    public void testGetResultReturnedError() throws Exception {
+    public void testGetResult_isOkWithHtmlData_ifReturnedDataIsOk() throws Exception {
 
         //given
         String instanceId = "Instance_1236678329433";
-        String scriptContent = getScriptContent("sample_scripts/status_error.sh");
+        String scriptContent = getScriptContent("sample_scripts/sample_ok_html.sh");
+        Shell shell = runScript(scriptContent, instanceId);
+
+        //when
+        JsonObject result = shell.getResult();
+
+        //then
+        assertEquals(STATUS_OK, result.getString(JSON_STATUS));
+        assertTrue(result.getString(JSON_DATA).contains("<table>"));
+    }
+
+    @Test
+    public void testGetResult_isError_ifReturnedDataIsError() throws Exception {
+
+        //given
+        String instanceId = "Instance_1236678329433";
+        String scriptContent = getScriptContent("sample_scripts/sample_error.sh");
         Shell shell = runScript(scriptContent, instanceId);
 
         //when
@@ -54,11 +72,11 @@ public class ShellTest {
     }
 
     @Test
-    public void testGetResultReturnedExecutionError() throws Exception {
+    public void testGetResult_isErrorWithMessage_ifErrorWhenExecuting() throws Exception {
 
         //given
         String instanceId = "Instance_1236678329433";
-        String scriptContent = getScriptContent("sample_scripts/status_execution_error.sh");
+        String scriptContent = getScriptContent("sample_scripts/sample_error_execution.sh");
         Shell shell = runScript(scriptContent, instanceId);
 
         //when
@@ -67,6 +85,66 @@ public class ShellTest {
         //then
         assertEquals(STATUS_ERROR, result.getString(JSON_STATUS));
         assertEquals(shell.getErrorOutput(), result.getString(JSON_MESSAGE));
+    }
+
+    @Test(expected = JSONException.class)
+    public void testGetResult_throwsException_ifReturnedDataIsInvalid() throws Exception {
+
+        //given
+        String instanceId = "Instance_1236678329433";
+        String scriptContent = getScriptContent("sample_scripts/sample_invalid.sh");
+        Shell shell = runScript(scriptContent, instanceId);
+
+        //when
+        shell.getResult();
+
+        //then
+        //expected Exception
+    }
+
+    @Test
+    public void testGetOutput_isOk_ifReturnedDataIsOk() throws Exception {
+
+        //given
+        String instanceId = "Instance_1236678329433";
+        String scriptContent = getScriptContent("sample_scripts/sample_ok.sh");
+        Shell shell = runScript(scriptContent, instanceId);
+
+        //when
+        String output = shell.getOutput();
+
+        //then
+        assertTrue(output.contains("{\"status\":\"ok\"}"));
+    }
+
+    @Test
+    public void testGetErrorOutput_isEmpty_ifReturnedDataIsOk() throws Exception {
+
+        //given
+        String instanceId = "Instance_1236678329433";
+        String scriptContent = getScriptContent("sample_scripts/sample_ok.sh");
+        Shell shell = runScript(scriptContent, instanceId);
+
+        //when
+        String output = shell.getErrorOutput();
+
+        //then
+        assertTrue(output.isEmpty());
+    }
+
+    @Test
+    public void testGetErrorOutput_isError_ifReturnedDataIsError() throws Exception {
+
+        //given
+        String instanceId = "Instance_1236678329433";
+        String scriptContent = getScriptContent("sample_scripts/sample_error.sh");
+        Shell shell = runScript(scriptContent, instanceId);
+
+        //when
+        String output = shell.getErrorOutput();
+
+        //then
+        assertTrue(output.contains("{\"status\":\"error\"}"));
     }
 
     private Shell runScript(String scriptContent, String instanceId) throws Exception {
