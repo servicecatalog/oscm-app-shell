dd2e5a8f31116279a5f6acdc7b8704542a9c15e3
Arkadiusz Kowalczyk <arkadiusz.kowalczyk@ts.fujitsu.com>
2018-12-18 12:48:01 +0100

    Added copyright headers

--- src/main/java/org/oscm/app/shell/business/api/ShellResultData.java
+++ src/main/java/org/oscm/app/shell/business/api/ShellResultData.java
@@ -1,3 +1,11 @@
+/*******************************************************************************
+ *
+ *  Copyright FUJITSU LIMITED 2018
+ *
+ *  Creation Date: Dec 18, 2018
+ *
+ *******************************************************************************/
+
 package org.oscm.app.shell.business.api;
 
 public class ShellResultData {
