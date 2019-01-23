4b5404c98cf3fc478e7cc4cadcb9c7042cc2f6eb
Lukasz Wiecek <Lukasz.Wiecek@ts.fujitsu.com>
2019-01-23 10:04:49 +0100

    Improve coverage (#49)
    * Remove JSON validation, remove support file.
    
    * Clean up ShellPoolTest class
    
    * Add unit tests
    
    * Clean up ShellPoolTest class
    
    * Add unit tests

--- src/test/java/org/oscm/app/shell/business/ShellControllerAccessTest.java
+++ src/test/java/org/oscm/app/shell/business/ShellControllerAccessTest.java
@@ -9,9 +9,15 @@
 package org.oscm.app.shell.business;
 
 import org.junit.Assert;
+import org.junit.Ignore;
 import org.junit.Test;
+import org.mockito.internal.util.reflection.Whitebox;
+import org.oscm.app.shell.ShellController;
+import org.oscm.app.v2_0.data.ControllerSettings;
 
 import java.util.List;
+
+import static org.mockito.Mockito.*;
 
 public class ShellControllerAccessTest {
 
@@ -27,7 +33,36 @@ public class ShellControllerAccessTest {
 
         //then
         Assert.assertEquals(size, list.size());
+    }
 
+    @Test
+    public void testGetSettings_skipRequestControllerSettings_ifSettingsNotNull() throws Exception {
+
+        //given
+        ShellControllerAccess shellControllerAccess = mock(ShellControllerAccess.class);
+        ControllerSettings controllerSettings = mock(ControllerSettings.class);
+        Whitebox.setInternalState(shellControllerAccess, "settings", controllerSettings);
+
+        //when
+        doNothing().when(shellControllerAccess).requestControllerSettings();
+        shellControllerAccess.getSettings();
+
+        //then
+        verify(shellControllerAccess, times(0)).requestControllerSettings();
+    }
+
+    @Ignore
+    public void testGetSettings_requestControllerSettings_ifSettingsNull() throws Exception {
+
+        //given
+        ShellControllerAccess shellControllerAccess = mock(ShellControllerAccess.class);
+        Whitebox.setInternalState(shellControllerAccess, "settings", null);
+
+        //when
+        shellControllerAccess.getSettings();
+
+        //then
+        verify(shellControllerAccess, times(1)).requestControllerSettings();
     }
 
 }
--- src/test/java/org/oscm/app/shell/business/api/ShellPoolTest.java
+++ src/test/java/org/oscm/app/shell/business/api/ShellPoolTest.java
@@ -2,44 +2,22 @@
  *
  *  Copyright FUJITSU LIMITED 2018                                           
  *
- *  Creation Date: Aug 2, 2017                                                      
+ *  Creation Date: 15.01.2019
  *
  *******************************************************************************/
 
 package org.oscm.app.shell.business.api;
 
-import org.junit.*;
+import org.junit.Assert;
+import org.junit.Test;
 import org.mockito.internal.util.reflection.Whitebox;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.List;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.*;
 import static org.oscm.app.shell.business.api.Shell.STATUS_ERROR;
-import static org.oscm.app.shell.business.api.ShellStatus.*;
-
-
-/**
- * FIXME
- */
+import static org.oscm.app.shell.business.api.ShellStatus.SUCCESS;
 
 public class ShellPoolTest {
-//    private ShellPool pool;
-//
-//    @Before
-//    public void before() {
-//            pool = new ShellPool();
-//            pool.initializeResources();
-//    }
-//
-//    @After
-//    public void after() {
-//        pool.releaseResources();
-//    }
 
     @Test
     public void testGetShellOutput_ReturnsOutput_ifFoundShellForLockId() throws Exception {
@@ -105,7 +83,6 @@ public class ShellPoolTest {
 
         //then
         Assert.assertEquals(STATUS_ERROR, shellResult.getStatus());
-
     }
 
     @Test(expected = ShellPoolException.class)
@@ -123,140 +100,101 @@ public class ShellPoolTest {
         Whitebox.setInternalState(shellPool, "shellPool", shellPoolList);
 
         //when
-        ShellResult result = shellPool.getShellResult("300");
+        shellPool.getShellResult("300");
 
         //then
         //exception is thrown
     }
 
-//    @Ignore
-//    public void runSingleCommand() throws Exception {
-//
-//        // given
-//        String instanceId = "myInstanceId";
-//        ShellCommand command = new ShellCommand("echo $?;sleep(2);echo \"END_OF_SCRIPT\"");
-//        String shellConsoleFile = "";
-//
-//        // when
-//        ShellStatus res = pool.runCommand(command, instanceId, shellConsoleFile);
-//
-//        // then
-//        assertEquals(ShellStatus.RUNNING, res);
-//        assertEquals(1, pool.shellPool.size());
-//        assertEquals(99, pool.shellctrl.availablePermits());
-//    }
-//
-//    @Ignore
-//    public void runSingleCommand_readShellOutput() throws Exception {
-//        // given
-//        String instanceId = "myInstanceId";
-//        ShellCommand command = new ShellCommand("echo $?;sleep(2);echo \"END_OF_SCRIPT\"");
-//        pool.runCommand(command, instanceId, "");
-//
-//        // when
-//        ShellStatus res;
-//        do {
-//            res = pool.consumeShellOutput(instanceId);
-//        } while (res == ShellStatus.RUNNING);
-//
-//        // then
-//        pool.unlockShell(instanceId);
-//        assertEquals(1, pool.shellPool.size());
-//        assertEquals(99, pool.shellctrl.availablePermits());
-//        assertEquals(ShellStatus.SUCCESS, res);
-//    }
-//
-//    @Ignore
-//    public void exitShellScript() throws Exception {
-//        String instanceId = "myInstanceId";
-//        ShellCommand command = new ShellCommand();
-//        command.init("sleep(1);exit;");
-//        String shellConsoleFile = "";
-//
-//        // when
-//        pool.runCommand(command, instanceId, shellConsoleFile);
-//        ShellStatus res = pool.consumeShellOutput(instanceId);
-//
-//        // then
-//        assertEquals(ShellStatus.RUNNING, res);
-//        pool.terminateShell(instanceId);
-//        assertEquals(0, pool.shellPool.size());
-//        assertEquals(100, pool.shellctrl.availablePermits());
-//    }
-//
-//    @Ignore
-//    public void useDifferentShells() throws Exception {
-//        // given
-//        ShellCommand cmd1 = new ShellCommand("echo $?;\r\nsleep(5);\r\necho \"END_OF_SCRIPT\"");
-//        ShellCommand cmd2 = new ShellCommand("echo $?;\r\nsleep(6);\r\necho \"END_OF_SCRIPT\"");
-//        ShellCommand cmd3 = new ShellCommand("echo $?;\r\nsleep(7);\r\necho \"END_OF_SCRIPT\"");
-//        String consoleFile = "";
-//        pool.runCommand(cmd1, "id1", consoleFile);
-//        pool.runCommand(cmd2, "id2", consoleFile);
-//        pool.runCommand(cmd3, "id3", consoleFile);
-//
-//        // when
-//        long ref = System.currentTimeMillis();
-//        long runtime = 0;
-//        boolean done = false;
-//        do {
-//            ShellStatus res1 = pool.consumeShellOutput("id1");
-//            assertTrue(res1 == SUCCESS || res1 == RUNNING);
-//
-//            ShellStatus res2 = pool.consumeShellOutput("id2");
-//            assertTrue(res2 == SUCCESS || res2 == RUNNING);
-//
-//            ShellStatus res3 = pool.consumeShellOutput("id3");
-//            assertTrue(res3 == SUCCESS || res3 == RUNNING);
-//
-//            done = (res1 == SUCCESS) && (res2 == SUCCESS) && (res3 == SUCCESS);
-//            // done = (res1 == SUCCESS);
-//            runtime = System.currentTimeMillis() - ref;
-//        } while (!done && runtime < 20000);
-//
-//        // then
-//        assertTrue(runtime < 20000);
-//        assertEquals(3, pool.shellPool.size());
-//        assertEquals(97, pool.shellctrl.availablePermits());
-//    }
-//
-//    @Ignore
-//    public void reuseShell() throws Exception {
-//        ShellCommand cmd1 = new ShellCommand("echo $?;echo \"END_OF_SCRIPT\"");
-//        ShellCommand cmd2 = new ShellCommand("echo $?;echo \"END_OF_SCRIPT\"");
-//        String consoleFile = "";
-//        ShellStatus res = pool.runCommand(cmd1, "id1", consoleFile);
-//        assertEquals(ShellStatus.RUNNING, res);
-//
-//        do {
-//            res = pool.consumeShellOutput("id1");
-//            assertTrue(res == SUCCESS || res == RUNNING);
-//        } while (res != SUCCESS);
-//
-//        pool.unlockShell("id1");
-//
-//        res = pool.runCommand(cmd2, "id2", consoleFile);
-//        assertEquals(ShellStatus.RUNNING, res);
-//
-//        do {
-//            res = pool.consumeShellOutput("id2");
-//            assertTrue(res == SUCCESS || res == RUNNING);
-//        } while (res != SUCCESS);
-//
-//    }
-//
-//    @Ignore
-//    public void runFailingCommand() throws Exception {
-//        ShellCommand cmd1 = new ShellCommand("unknown_command;echo \"END_OF_SCRIPT\"");
-//        String consoleFile = "";
-//        ShellStatus res = pool.runCommand(cmd1, "id1", consoleFile);
-//
-//        do {
-//            res = pool.consumeShellOutput("id1");
-//            assertTrue(res.name(), res == PSSHELL_ERROR || res == RUNNING);
-//        } while (res != PSSHELL_ERROR);
-//
-//        pool.unlockShell("id1");
-//    }
+    @Test
+    public void testConsumeShellOutput_returnsSuccess_ifReturnCodeSuccess() throws Exception {
+
+        //given
+        ShellCommand shellCommand = new ShellCommand();
+        shellCommand.setReturnCode(SUCCESS);
+        shellCommand.addOutputLine("output string");
+        Shell shell = new Shell();
+        Whitebox.setInternalState(shell, "command", shellCommand);
+        shell.setLockId("100");
+        ArrayList<Shell> shellPoolList = new ArrayList<>();
+        shellPoolList.add(shell);
+        ShellPool shellPool = new ShellPool();
+        Whitebox.setInternalState(shellPool, "shellPool", shellPoolList);
+
+        //when
+        ShellStatus result = shellPool.consumeShellOutput("100");
+
+        //then
+        Assert.assertEquals(SUCCESS, result);
+    }
+
+    @Test(expected = ShellPoolException.class)
+    public void testConsumeShellOutput_throwsException_ifLockIdNotFound() throws Exception {
+
+        //given
+        ShellCommand shellCommand = new ShellCommand();
+        shellCommand.addOutputLine("output string");
+        Shell shell = new Shell();
+        Whitebox.setInternalState(shell, "command", shellCommand);
+        shell.setLockId("100");
+        ArrayList<Shell> shellPoolList = new ArrayList<>();
+        shellPoolList.add(shell);
+        ShellPool shellPool = new ShellPool();
+        Whitebox.setInternalState(shellPool, "shellPool", shellPoolList);
+
+        //when
+        shellPool.consumeShellOutput("300");
+
+        //then
+        //exception is thrown
+    }
+
+    @Test
+    public void testGetShellErrorOutput_returnsOutput_ifFoundShellForLockId() throws Exception {
+
+        //given
+        ShellCommand shellCommand = new ShellCommand();
+        shellCommand.addOutputLine("output string");
+        ArrayList<String> errorOutput = new ArrayList<>();
+        errorOutput.add("Error output");
+        Whitebox.setInternalState(shellCommand, "errorOutput", errorOutput);
+        Shell shell = new Shell();
+        Whitebox.setInternalState(shell, "command", shellCommand);
+        shell.setLockId("100");
+        ArrayList<Shell> shellPoolList = new ArrayList<>();
+        shellPoolList.add(shell);
+        ShellPool shellPool = new ShellPool();
+        Whitebox.setInternalState(shellPool, "shellPool", shellPoolList);
+
+        //when
+        String result = shellPool.getShellErrorOutput("100");
+
+        //then
+        Assert.assertEquals("Error output\n\t", result);
+    }
+
+    @Test
+    public void testGetShellErrorOutput_returnsEmptyString_ifLockIdNotFound() throws Exception {
+
+        //given
+        ShellCommand shellCommand = new ShellCommand();
+        shellCommand.addOutputLine("output string");
+        ArrayList<String> errorOutput = new ArrayList<>();
+        errorOutput.add("Error output");
+        Whitebox.setInternalState(shellCommand, "errorOutput", errorOutput);
+        Shell shell = new Shell();
+        Whitebox.setInternalState(shell, "command", shellCommand);
+        shell.setLockId("100");
+        ArrayList<Shell> shellPoolList = new ArrayList<>();
+        shellPoolList.add(shell);
+        ShellPool shellPool = new ShellPool();
+        Whitebox.setInternalState(shellPool, "shellPool", shellPoolList);
+
+        //when
+        String result = shellPool.getShellErrorOutput("200");
+
+        //then
+        Assert.assertEquals("", result);
+    }
 
 }
