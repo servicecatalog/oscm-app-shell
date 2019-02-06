783cd9b288bb15aec586302dc66f51b49a91b53f
Arkadiusz Kowalczyk <arkadiusz.kowalczyk@ts.fujitsu.com>
2019-02-06 11:53:23 +0100

    Verification script result handling

--- src/test/java/org/oscm/app/shell/ShellControllerTest.java
+++ src/test/java/org/oscm/app/shell/ShellControllerTest.java
@@ -58,6 +58,7 @@ public class ShellControllerTest {
 
         // given
         ProvisioningSettings settings = getProvisioningSettings();
+        doNothing().when(controller).runVerificationScript(any(Configuration.class));
 
         // when
         InstanceDescription id = controller.createInstance(settings);
@@ -191,11 +192,26 @@ public class ShellControllerTest {
         verify(controllerAccess, times(1)).storeSettings(any(ControllerSettings.class));
     }
 
+    @Test(expected = APPlatformException.class)
+    public void testRunVerificationScript_throwsException_ifScriptFileIsNotFound() throws Exception {
+
+        //given
+        Configuration configuration = new Configuration(getProvisioningSettings());
+        doNothing().when(validator).validate(configuration, VERIFICATION_SCRIPT);
+
+        // when
+        controller.runVerificationScript(configuration);
+
+        //then
+        verify(validator, times(1)).validate(configuration, VERIFICATION_SCRIPT);
+    }
+
 
 
     private ProvisioningSettings getProvisioningSettings() {
 
         String sampleScriptPath = "/some/path/script.sh";
+        String instanceId = "Instance_323213213213";
 
         HashMap<String, Setting> parameters = new HashMap<>();
         parameters.put(PROVISIONING_SCRIPT.name(), new Setting(PROVISIONING_SCRIPT.name(), sampleScriptPath));
@@ -204,6 +220,10 @@ public class ShellControllerTest {
         parameters.put(ASSIGN_USER_SCRIPT.name(), new Setting(ASSIGN_USER_SCRIPT.name(), sampleScriptPath));
         parameters.put(DEASSIGN_USER_SCRIPT.name(), new Setting(DEASSIGN_USER_SCRIPT.name(), sampleScriptPath));
         parameters.put(CHECK_STATUS_SCRIPT.name(), new Setting(CHECK_STATUS_SCRIPT.name(), sampleScriptPath));
+
+        parameters.put(VERIFICATION_SCRIPT.name(), new Setting(VERIFICATION_SCRIPT.name(), sampleScriptPath));
+        parameters.put(VERIFICATION_TIMEOUT.name(), new Setting(VERIFICATION_TIMEOUT.name(), "600"));
+        parameters.put(INSTANCE_ID.name(), new Setting(INSTANCE_ID.name(), instanceId));
 
         HashMap<String, Setting> emptyMap = new HashMap<>();
 
