73228c15fe5b7a4abe453e3f3718a852c1086519
Lorenz Goebel <lorenzgoebel@gmx.de>
2019-02-14 16:02:53 +0100

    Added usage_data.sh

---
+++ src/main/resources/sample_scripts/advanced_scripts/usage_data.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo '
+	{"status":"ok",
+	 "message":"Script execution is successful",
+	 "usageData": [
+         {
+           "eventId": "EVENT_DISK_GIGABYTE_HOURS",
+           "multiplier": 0
+         },
+         {
+           "eventId": "EVENT_CPU_HOURS",
+           "multiplier": 0
+         },
+         {
+           "eventId": "EVENT_RAM_MEGABYTE_HOURS",
+           "multiplier": 0
+         }
+     ]}'
+ 
+echo "END_OF_SCRIPT";
