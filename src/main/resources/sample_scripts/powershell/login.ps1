e904c2f1f81a80d6cc528c67f1b472bfdb8ab29f
kowalczyka <arkadiusz.kowalczyk@ts.fujitsu.com>
2020-07-31 09:44:41 +0200

    Login and sample status scripts

---
+++ src/main/resources/sample_scripts/powershell/login.ps1
@@ -0,0 +1,49 @@
+Function Check-Token{
+    Param($AzureContext)
+    $cacheItems = $azureContext.TokenCache.ReadItems()
+    $tokenExpirationDate = $cacheItems.ExpiresOn.DateTime
+    $tokenExpired = $tokenExpirationDate -lt (Get-Date)
+    return $tokenExpired
+}
+
+Function Check-AzureParam{
+    Param($AzureParameter, $AzureParameterName)
+    if($AzureParameter -eq $null){
+        throw "Custom attribute $AzureParameterName is not defined"
+    }
+}
+
+$ErrorActionPreference = 'Stop'
+$loginError = $false
+
+Try {
+    Check-AzureParam -AzureParameter $AzureApplicationId -AzureParameterName "AzureApplicationId"
+    Check-AzureParam -AzureParameter $AzureTenantId -AzureParameterName "AzureTenantId"
+    Check-AzureParam -AzureParameter $AzureSubscriptionId -AzureParameterName "AzureSubscriptionId"
+    Check-AzureParam -AzureParameter $AzureSecret -AzureParameterName "AzureSecret"
+
+    $azureContext = Get-AzContext
+
+    if($azureContext -eq $null -Or (Check-Token -AzureContext $azureContext)) {
+        $password = ConvertTo-SecureString -String $AzureSecret -AsPlainText
+        $credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $AzureApplicationId, $password
+        $connection = Connect-AzAccount -Credential $credential -Tenant $AzureTenantId -ServicePrincipal -WarningAction Ignore
+    }
+
+    if($azureContext.Subscription.Id -ne $AzureSubscriptionId){
+        $logout = Logout-AzAccount
+        throw "AzureSubscriptionId is invalid. Please check custom attribute value"
+    }
+
+    if($azureContext.Tenant.Id -ne $AzureTenantId){
+        $logout = Logout-AzAccount
+        throw "AzureTenantId is invalid. Please check custom attribute value"
+    }
+}
+Catch {
+    $loginError = $true
+    $errorCommand = $PSItem.InvocationInfo.MyCommand
+    $errorMsg = $_.Exception.Message -replace "'",""
+    Write-Output "{'status':'error','message':'$($errorCommand): $($errorMsg)'}"
+    Write-Output "END_OF_SCRIPT"
+}
